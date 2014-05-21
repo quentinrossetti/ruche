23b902ac87ee66b70f1dbd589f4046b48ef44f4a
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-05-21 18:49:52 +0200

    Testing skeleton

---
+++ test/cli/test-install.js
@@ -0,0 +1,15 @@
+/*
+ * Specification: install command
+ */
+
+// Module dependencies
+var expect = require('chai').expect;
+
+// Test suite
+describe('cli:install', function() {
+  
+  it('should be called with a package', function() {
+    
+  });
+
+});
---
+++ test/ruche/test-install.js
@@ -0,0 +1,24 @@
+/*
+ * Specification: install function
+ */
+
+// Module dependencies
+var expect = require('chai').expect;
+var ruche = require('../../lib/ruche');
+
+// Test suite
+describe('ruche:install', function() {
+  
+  it('should read the package list', function() {
+    
+  });
+  
+  it('should create a package directory in %RUCHE_TOOLS%', function() {
+    
+  });
+
+  it('should create a shortcut in %PATH%', function() {
+    
+  });
+
+});
