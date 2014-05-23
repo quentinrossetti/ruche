4454d366dbeb17537b6d6a449a933aaa4dc18553
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-05-23 12:58:38 +0200

    ruche object has now an emitter object in it (REPL only)

--- bin/ruche-repl.js
+++ bin/ruche-repl.js
@@ -4,7 +4,9 @@
 var debug = require('debug')('ruche:repl');
 var repl = require('repl');
 var ruche = require('../lib/ruche');
+var emitter = require('../lib/ruche/util/emitter');
 
 // Start the REPL with ruche available
 var prompt = repl.start({ prompt: 'ruche> ' });
 prompt.context.ruche = ruche;
+prompt.context.ruche.emitter = emitter;
