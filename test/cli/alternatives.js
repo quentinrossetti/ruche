bb9d59599dc11bf57840781219edfa67a231e749
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-10 13:10:02 +0200

    Alternatives CLI lists the available versions

---
+++ test/cli/alternatives.js
@@ -0,0 +1,34 @@
+// Module dependencies
+var debug = require('debug')('ruche:test:cli:atlernatives');
+var expect = require('chai').expect;
+var cli = require('../../lib/cli');
+var fake = require('./util/fake');
+
+/**
+ * Test suite for cli:alternatives
+ *
+ * Compare the results of `cli.argv(command)` against expected scenarios. 
+ */
+describe('cli:alternatives', function () {
+  
+  it('alternatives package should produce `alternatives` context', function () {
+    var argv = fake.argv('ruche alternatives package');
+    var result = cli.argv(argv);
+    expect(result.context).to.equal('alternatives');
+  });
+
+  it('With no package should produce `alternatives` help', function () {
+    var argv = fake.argv('ruche alternatives');
+    var result = cli.argv(argv);
+    expect(result.context).to.equal('help');
+    expect(result.help).to.equal('alternatives');
+  });
+
+  it('should be called with only one package', function () {
+    var argv = fake.argv('ruche alternatives pack1 pack2');
+    var result = cli.argv(argv);
+    expect(result.context).to.equal('help');
+    expect(result.help).to.equal('alternatives');
+  });
+  
+});
