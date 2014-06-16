2f0f6ea150ab4c41bfdccbea56a263ac2505ffba
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-16 14:03:05 +0200

    Fix: Missing dependencies

--- bin/repl.js
+++ bin/repl.js
@@ -2,8 +2,8 @@
 
 // Module dependencies
 var debug   = require('debug')('ruche:repl');
+var repl    = require('repl');
 var ruche   = require('../lib/ruche');
-var emitter = require('../lib/ruche/util/emitter');
 
 // Start the REPL with ruche available
 var prompt = repl.start({ prompt: 'ruche> ' });
