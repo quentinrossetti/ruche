e93fbb89fa6b2d1a5d503ae87538864ed808c8c6
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-17 08:32:36 +0200

    100% test coverage for cli version and help

--- test/fixture/index.js
+++ test/fixture/index.js
@@ -1,8 +1,12 @@
+// Module dependencies
+'use strict';
+
 var fixture = {
   argv  : require('./argv'),
   env   : require('./env'),
   error : require('./error'),
+  stdout: require('./stdout'),
   server: require('./server'),
-}
+};
 
 module.exports = fixture;
