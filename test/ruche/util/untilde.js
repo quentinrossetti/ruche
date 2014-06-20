ed308617b3be126abc23442834c762dc7ff6e93d
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-20 09:03:33 +0200

    Tests and doc for RucheUtil.untilde()
    Renaming the file and references.

---
+++ test/ruche/util/untilde.js
@@ -0,0 +1,41 @@
+// Module dependencies
+/* global describe, it, before, after, beforeEach, afterEach */
+'use strict';
+var debug   = require('debug')('ruche:test:ruche:util');
+var chai    = require('chai');
+var path    = require('path');
+var untilde = require('../../../lib/ruche/util/untilde');
+
+// Initialization and configuration
+var expect = chai.expect;
+
+/**
+ * Test suite for ruche:util:untilde
+ */
+describe('ruche:util:untilde', function () {
+
+  before(function () {
+    debug('Test suite for ruche:util:untilde started');
+
+  });
+  after(function () {
+    debug('Test suite for ruche:util:untilde ended');
+  });
+
+  it('~ should returns the user home', function () {
+    var r = untilde('~');
+    var home = process.env.HOME || process.env.USERPROFILE;
+    expect(r).to.eql(home);
+  });
+
+  it('@ should returns the user home', function () {
+    var r = untilde('@');
+    expect(r).to.eql(path.resolve(__dirname, '../../..'));
+  });
+
+  it('should act like path.resolve()', function () {
+    var r = '../../fixture';
+    expect(untilde(r)).to.eql(path.resolve(r));
+  });
+
+});
