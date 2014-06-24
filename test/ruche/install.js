b3d8bca6db7eefb11af579128e43d12adf7fcab0
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-24 14:25:16 +0200

    Add test: should send an error when unable to find package info

--- test/ruche/install.js
+++ test/ruche/install.js
@@ -3,6 +3,7 @@
 'use strict';
 var debug   = require('debug')('ruche:test:ruche');
 var chai    = require('chai');
+var fs      = require('fs');
 var ruche   = require('../../lib/ruche');
 var fixture = require('../fixture');
 
@@ -31,7 +32,7 @@ describe('ruche:install', function () {
     });
   });
 
-  it('should an error when unable to find a package information', function (done) {
+  it('should send an error when unable to find package info', function (done) {
     ruche.install(['invalid'], function (err, data) {
       if (err) {
         done();
@@ -42,6 +43,20 @@ describe('ruche:install', function () {
   it('should an error when unable to find a package', function (done) {
     ruche.install(['fixpack-0.0.1-win64'], function (err, data) {
       if (err) {
+        done();
+      }
+    });
+  });
+
+  it('should an error when unable to extract a package', function (done) {
+    var emitter = require('../../lib/ruche/util/emitter');
+    var untilde = require('../../lib/ruche/util/untilde');
+    emitter.on('dl-done-0', function () {
+      fs.unlinkSync(untilde('@/.tmp/test/tmp/fixpack-0.0.1-win32.tar.gz'));
+    });
+    ruche.install(['fixpack-0.0.1-win32'], function (err, data) {
+      if (err) {
+        emitter.removeAllListeners('dl-done-0');
         done();
       }
     });
