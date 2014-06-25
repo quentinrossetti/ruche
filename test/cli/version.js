ca5c41b71176c962e4554e4a2b86bc8c56911ac1
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-06-25 17:00:47 +0200

    Tests for cli/version

--- test/cli/version.js
+++ test/cli/version.js
@@ -5,9 +5,11 @@ var debug   = require('debug')('ruche:test:cli:version');
 var chai    = require('chai');
 var cli     = require('../../lib/cli');
 var fixture = require('../fixture');
+var emitter = require('../../lib/ruche/util/emitter');
 
 // Initialization and configuration
 var expect = chai.expect;
+var stdout = process.stdout;
 
 /**
  * Test suite for cli:version
@@ -16,35 +18,41 @@ describe('cli:version', function () {
 
   before(function () {
     fixture.env.before();
+    emitter.removeAllListeners();
     debug('Test suite for cli:version started');
   });
   after(function () {
     fixture.env.after();
+    emitter.removeAllListeners();
     debug('Test suite for cli:version ended');
   });
 
-  // it('should print a valid version to stdout', function(done) {
-  //   var capture = [];
-  //   fixture.stdout.before(capture);
-  //   /*jshint unused:false */
-  //   cli.version(function(data) {
-  //     fixture.stdout.after();
-  //     expect(capture[0]).to.match(/^v\d+.\d+.\d+\n$/);
-  //     done();
-  //   });
-  // });
+  it('should print a valid version to stdout', function(done) {
+    var capture = [];
+    stdout.hook('write', function(string, encoding, fd, write) {
+        capture.push(string);
+    }, true);
+    /*jshint unused:false */
+    cli.version(function(data) {
+      stdout.unhook('write');
+      expect(capture[0]).to.match(/^v\d+.\d+.\d+\n$/);
+      done();
+    });
+  });
 
-  // it('should send a error when unreachable package.json', function (done) {
-  //   var capture = [];
-  //   fixture.stdout.before(capture);
-  //   fixture.error.packageUnreachableBefore();
-  //   /*jshint unused:false */
-  //   cli.version(function(data) {
-  //     fixture.error.packageAfter();
-  //     fixture.stdout.after();
-  //     expect(capture[0]).to.have.string('Error:');
-  //     done();
-  //   });
-  // });
+  it('should send a error when unreachable package.json', function (done) {
+    var capture = [];
+    stdout.hook('write', function(string, encoding, fd, write) {
+        capture.push(string);
+    }, true);
+    fixture.error.packageUnreachableBefore();
+    /*jshint unused:false */
+    cli.version(function(data) {
+      fixture.error.packageAfter();
+      stdout.unhook('write');
+      expect(capture[0]).to.have.string('Error:');
+      done();
+    });
+  });
 
 });
